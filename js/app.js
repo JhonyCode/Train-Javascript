26e0a57c4182fb8f718c4499456c17b6edda4929
JhonyCode <jhonygane@gmail.com>
2022-04-20 21:02:37 +0200

    Adding js functions and sounds

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 window.addEventListener("load", () => {
-  // querying elements
+  //Selecting sounds
+  let intro = new Audio("sounds/intro-music.mp3");
+  let shot = new Audio("sounds/shot2.mp3");
+  //Selecting elements
+  let modal = document.querySelector(".modal");
+  let startButton = document.querySelector(".modal-content").lastElementChild;
   let playerScore = document.querySelector(".row.bottom").firstElementChild;
-  let cpuScore = document.querySelector(".row.bottom").lastElementChild;
+  // let cpuScore = document.querySelector("row.bottom").lastElementChild;
   let roundNumber = document.querySelector(".round");
   let display = document.querySelector(".display");
   let userMove =
     document.querySelector(".results").firstElementChild.firstElementChild;
-  let cpuMove =
-    document.querySelector(".results").lastElementChild.firstElementChild;
-
+  let cpuMove = document.querySelector(".results").lastElementChild;
   let option1 =
     document.querySelector(".play").firstElementChild.nextElementSibling;
   let option2 = option1.nextElementSibling;
   let option3 = option2.nextElementSibling;
-//   console.log(option3);
+  let userChoices = [option1, option2, option2];
+
+  startButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    intro.play();
+  });
+  userChoices.forEach(() => {
+    choice.addEventListener("click", () => {
+      shot.play();
+    });
+  });
 });
